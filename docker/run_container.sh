aaf9b3057e9ba0492b1ae5c4fbfd616dcf7c09da
Joao Gueifao <1582944+joao-gueifao-924@users.noreply.github.com>
2025-03-19 15:06:59 -0300

    Enhance Docker setup by moving NVIDIA_DISABLE_REQUIRE and improving securty and readability in run_container.sh script

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
 docker rm -f foundationpose
 DIR=$(pwd)/../
-xhost +  && docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $DIR:$DIR -v /home:/home -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp  --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE foundationpose:latest bash -c "cd $DIR && bash"
+
+xhost +local:docker &&\
+docker run \
+    --gpus all \
+    -it \
+    --name foundationpose \
+    -v $DIR:$DIR \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=${DISPLAY} \
+    foundationpose_custom:latest \
+    bash -c "cd $DIR && bash"
