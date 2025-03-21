ae06996980d27b55da5ca6a3f368bf8af928c874
joao-gueifao-924 <1582944+joao-gueifao-924@users.noreply.github.com>
2025-03-20 21:37:25 -0300

    Docker build changes (#1)

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,10 @@
 DIR=$(pwd)
 
 cd $DIR/mycpp/ && mkdir -p build && cd build && cmake .. -DPYTHON_EXECUTABLE=$(which python) && make -j11
-cd /kaolin && rm -rf build *egg* && pip install -e .
-cd $DIR/bundlesdf/mycuda && rm -rf build *egg* && pip install -e .
+
+# Disable Kaolin and BundleSDF as we are not doing model-free object pose estimation.
+# We currently only perform object pose estimation given their 3D models are provided as input. 
+#cd /kaolin && rm -rf build *egg* && pip install -e .
+#cd $DIR/bundlesdf/mycuda && rm -rf build *egg* && pip install -e .
 
 cd ${DIR}
--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,3 +1,14 @@
-docker rm -f foundationpose
+#!/bin/bash
+
+docker rm -f foundationpose 2>/dev/null || true
 DIR=$(pwd)/../
-xhost +  && docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $DIR:$DIR -v /home:/home -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp  --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE foundationpose:latest bash -c "cd $DIR && bash"
+
+xhost +local:docker &&\
+docker run \
+    --gpus all \
+    -it \
+    --name foundationpose \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=${DISPLAY} \
+    foundationpose_custom:latest
+
